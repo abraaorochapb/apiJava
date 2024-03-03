2927b9ae4be5a99af8d91ddeb7541fc3db52b791
abraaorochapb <abraaorochapb@gmail.com>
2024-03-03 15:15:59 -0300

    Adiciona rotas de consulta por id, put e delete

--- src/main/java/com/abraaorochapb/apiJava/controllers/ProductController.java
+++ src/main/java/com/abraaorochapb/apiJava/controllers/ProductController.java
@@ -4,9 +4,13 @@ import com.abraaorochapb.apiJava.domain.product.Product;
 import com.abraaorochapb.apiJava.domain.product.ProductRepository;
 import com.abraaorochapb.apiJava.domain.product.ValidProduct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 
 @RestController
@@ -21,11 +25,41 @@ public class ProductController {
         return ResponseEntity.ok(allProducts);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity getProduct(@PathVariable Long id) {
+        return ResponseEntity.ok(productRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado com o ID: " + id)));
+    }
+
     @PostMapping
     public ResponseEntity saveProduct(@RequestBody @Validated ValidProduct validProduct) {
         Product product = new Product(validProduct);
         System.out.println(product);
         productRepository.save(product);
         return ResponseEntity.ok(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteProduct(@PathVariable Long id) {
+        if (!productRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        productRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateProduct(@PathVariable Long id, @RequestBody ValidProduct validProduct) {
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        if (optionalProduct.isPresent()) {
+            Product existingProduct = optionalProduct.get();
+            existingProduct.setNome(validProduct.nome());
+            existingProduct.setDescricao(validProduct.descricao());
+            existingProduct.setPreco(validProduct.preco());
+            productRepository.save(existingProduct);
+            return ResponseEntity.ok(existingProduct);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
